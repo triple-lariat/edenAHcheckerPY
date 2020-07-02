51d95dd1d01669ff4f6af743564d62ba772ba777
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-01 20:23:36 -0700

    added help descriptions for yellbot commands

--- bot.py
+++ bot.py
@@ -91,8 +91,15 @@ async def b(ctx, *, message:str):
 
         await ctx.send(embed=helper.build_bazaar_embed(item_name, page_exist))
 
-@bot.command(aliases=['y'])
+#@bot.command(aliases=['y'])
+#async def yells(ctx):
+#        await ctx.send(embed=helper.build_yell_embed())
+
+# Add help details for yellbot.py commands.
+@bot.command()
 async def yells(ctx):
-        await ctx.send(embed=helper.build_yell_embed())
+        '''Enables live yell chat log in the channel this command is used in.
+           Usage: !yells [on|off]'''
+        return
 
 bot.run(eden_bot_token)
