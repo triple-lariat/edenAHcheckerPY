ab48190444a4737bd6cc34e5ca6d10d79ef2ec82
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-01 20:24:57 -0700

    added error handler and multiple channel capability

--- yellbot.py
+++ yellbot.py
@@ -1,8 +1,9 @@
 import discord
 import asyncio
 import requests as r
+from discord.ext import commands
 from datetime import datetime
-import json, ast, pytz, platform, time
+import json, ast, pytz, platform, time, sys
 import edenAHhelper as helper
 
 try:
@@ -24,6 +25,21 @@ class MyClient(discord.Client):
         print(self.user.id)
         print('------')
 
+    async def on_message(self, message):
+            if message.author == self.user:
+                    return
+            if message.content.startswith('!yells on'):
+                    helper.add_yell_channel(message.channel.id)
+                    await message.channel.send('Yell messages will be sent to this channel.')
+                    return
+            if message.content.startswith('!yells off'):
+                    helper.del_yell_channel(message.channel.id)
+                    await message.channel.send('Yell messages have been disabled for this channel.')
+                    return
+            if message.content.startswith('!error'):
+                    a = self.get_channel(728081585441603615)
+                    await a.send('task failed successfully')
+
     async def my_background_task(self):
         await self.wait_until_ready()
         # togglable var for logging yells sent
@@ -31,26 +47,44 @@ class MyClient(discord.Client):
         counter = 0
         yell_history = [{} for i in range(31)]
 
-        channel = self.get_channel(727691425688453150) # channel ID goes here
-        while not self.is_closed():
-            #print(f"[{helper.get_my_timestamp_now()}] cycle started")
-            if log_yells:
-                log = open('eden_yell_log.txt','a')
-            yell_tuple = helper.yell_helper(yell_history)
-            yell_history = yell_tuple[1]
-            for yell in yell_tuple[0]:
-                f_yell = helper.yell_formatter(yell)
-                name = f_yell['speaker']
-                message = f_yell['message']
-                date = f_yell['date']
-                yell_message = f'[{date}] **{name}**: {message}'
-                if log_yells:
-                    log.write(yell_message + '\n')
-                await channel.send(yell_message)
-            counter += 1
-            print('cycle ' + f'{counter}' +  ' complete')
-            log.close()
-            await asyncio.sleep(30) # task runs every 30 seconds
+        #channel = self.get_channel(727691425688453150) # channel ID goes here
+        try:
+                while not self.is_closed():
+                    #print(f"[{helper.get_my_timestamp_now()}] cycle started")
+                    channel_ids = helper.get_yell_channels()
+                    if log_yells:
+                        log = open('eden_yell_log.txt','a')
+                    yell_tuple = helper.yell_helper(yell_history)
+                    yell_history = yell_tuple[1]
+                    for yell in yell_tuple[0]:
+                        f_yell = helper.yell_formatter(yell)
+                        name = f_yell['speaker']
+                        message = f_yell['message']
+                        date = f_yell['date']
+
+                        yell_message = f'[{date}] **{name}**: {message}'
+
+                        if log_yells:
+                            log.write(yell_message + '\n')
+
+                        for channel in channel_ids:
+                                try:
+                                        to_send = self.get_channel(channel)
+                                        await to_send.send(yell_message)
+                                except AttributeError:
+                                        print(sys.exc_info())
+                                        helper.del_yell_channel(channel)
+                                        print(f'Purged invalid channel {channel}')
+                    counter += 1
+                    print('cycle ' + f'{counter}' +  ' complete')
+                    log.close()
+                    await asyncio.sleep(30) # task runs every 30 seconds
+        except Exception:
+                print('Background task raised exception')
+                print(sys.exc_info())
+                raise
+        finally:
+                print('yell loop exited')
 
 
 client = MyClient()
