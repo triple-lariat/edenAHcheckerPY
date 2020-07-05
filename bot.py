029de8781385f71c35a01902157bf6b9cd2459fd
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-04 20:30:46 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,117 @@
+import platform
+
+import discord
+from discord.ext import commands
+
+import edenAHhelper as helper
+
+# import bot token from separate file 'eden_bot_token.py'
+try:
+    from eden_bot_token import eden_bot_token
+except ImportError:
+    new_instance = open('eden_bot_token.py', 'w')
+    new_instance.write('eden_bot_token = "YoUrBotTokEn"')
+    raise ImportError('Please enter your bot token in eden_bot_token.py' +
+                      '\nThis file has been created for you.')
+
+description = ''
+bot = commands.Bot(command_prefix='!', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('edenAHbot Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(activity=discord.Activity(name=platform.system(),
+                                                        type=discord.ActivityType.playing))
+
+
+@bot.command()
+async def getid(ctx, *, message: str):
+    '''Get the item ID and homepointxi link for any item
+                (filtered out some OOE items)'''
+    message = message.split(' ')
+    separator = '_'
+    item_name = separator.join(message)
+    csv = open('item_names_and_ids.csv', 'r')
+    item_info = []
+    for line in csv:
+        item_info.append(line.split(','))
+    i = 0
+    try:
+        while not (item_info[i][1].rstrip() == item_name):
+            i += 1
+        await ctx.channel.send(item_info[i])
+        await ctx.channel.send(f'http://homepointxi.com/db/items/{item_info[i][0]}/{item_name}')
+    except IndexError:
+        await ctx.channel.send('No item found')
+    csv.close()
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command()
+async def args(ctx, *, message: str):
+    message = message.split(' ')
+    for item in enumerate(message):
+        await ctx.send(message[item[0]])
+
+
+@bot.command()
+async def ah(ctx, *, message: str):
+    '''Gets the AH entries for an item from the website.
+            Usage: !ah [item name] [y for stack]'''
+    # split the arguments given with command into an array
+    message = message.split(' ')
+    separator = '_'
+    # check whether to look up a stack or single of given item
+    stack_flag = 'false'
+    if message[-1] == 'y':
+        stack_flag = 'true'
+        item_name = separator.join(message[:-1])
+    else:
+        item_name = separator.join(message)
+
+    # check if the item given actually exists
+    page_exist = helper.check_item(item_name)
+    stack_flag = page_exist[1]
+    
+    await ctx.send(embed=helper.build_AH_embed(item_name, page_exist[0], stack_flag))
+
+
+@bot.command(aliases=['bazaar'])
+async def b(ctx, *, message: str):
+    '''Gets the bazaar entries for an item. May take a while...
+           Usage: !b [item name]
+           Note: the "x2" or other number beside an entry shows how many
+           individual item slots, not the actual amount.'''
+    # split the arguments given with command into an array
+    message = message.split(' ')
+    separator = '_'
+    item_name = separator.join(message)
+
+    page_exist = helper.check_item(item_name)
+
+    await ctx.send(embed=helper.build_bazaar_embed(item_name, page_exist))
+
+
+# @bot.command(aliases=['y'])
+# async def yells(ctx):
+#        await ctx.send(embed=helper.build_yell_embed())
+
+# Add help details for yellbot.py commands.
+@bot.command()
+async def yells(ctx):
+    '''Enables live yell chat log in the channel this command is used in.
+           Usage: !yells [on|off]'''
+    return
+
+try:
+    bot.run(eden_bot_token)
+except discord.LoginFailure:
+    print('There was an error logging in. Please ensure you have the right login token set in eden_bot_token.py')
---
+++ yellbot.py
@@ -0,0 +1,92 @@
+import discord
+import asyncio
+import requests as r
+from discord.ext import commands
+from datetime import datetime
+import json, ast, pytz, platform, time, sys
+import edenAHhelper as helper
+
+try:
+    from eden_bot_token import eden_bot_token
+except ImportError:
+    raise ImportError('You must create a file "eden_bot_token.py" with' +
+                      ' variable eden_bot_token = "your.bot.token"')
+
+
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.my_background_task())
+
+    async def on_ready(self):
+        print('yellbot.py logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('!yells on'):
+            if message.author.permissions_in(message.channel).manage_messages:
+                helper.add_yell_channel(message.channel.id)
+                await message.channel.send('Yell messages will be sent to this channel.')
+            else:
+                await message.channel.send(f'{message.author.mention}' +
+                                           ' you must have "manage messages" permissions to use this command!')
+            return
+        if message.content.startswith('!yells off'):
+            if message.author.permissions_in(message.channel).manage_messages:
+                helper.del_yell_channel(message.channel.id)
+                await message.channel.send('Yell messages have been disabled for this channel.')
+            else:
+                await message.channel.send(f'{message.author.mention}' +
+                                           ' you must have "manage messages" permissions to use this command!')
+            return
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        # toggleable var for logging yells sent
+        log_yells = True
+        counter = 0
+        yell_history = [{} for i in range(31)]
+
+        try:
+            while not self.is_closed():
+                if helper.check_channels_exist():
+                    print(f"[{helper.get_my_timestamp_now()}] cycle started.")
+                    channel_ids = helper.get_yell_channels()
+                    if log_yells:
+                        log = open('eden_yell_log.txt', 'a')
+                    yell_tuple = helper.get_new_yells(yell_history)
+                    yell_history = yell_tuple[1]
+                    for yell in yell_tuple[0]:
+                        yell_message = helper.yell_formatter(yell)
+
+                        if log_yells:
+                            log.write(yell_message + '\n')
+
+                        for channel in channel_ids:
+                            try:
+                                to_send = self.get_channel(channel)
+                                await to_send.send(yell_message)
+                            except AttributeError:
+                                print(sys.exc_info())
+                                helper.del_yell_channel(channel)
+                                print(f'Purged invalid channel {channel}')
+                    counter += 1
+                    print(f'[{helper.get_my_timestamp_now()}] cycle {counter} complete.')
+                    log.close()
+                await asyncio.sleep(30)  # task runs every 30 seconds
+        except Exception:
+            print('Background task raised exception')
+            print(sys.exc_info())
+            raise
+        finally:
+            print('yell loop exited')
+
+
+client = MyClient()
+client.run(eden_bot_token)
