0c70412af258851671696fde6e19fc74e4d90615
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-08 22:16:44 -0700

    completely reorganized yells loop
    changed yellbot as a whole to a cog rather than a separate bot client. loop is now structured differently to account for this change

--- yellbot_rewrite.py
+++ yellbot_rewrite.py
@@ -13,6 +13,11 @@ import edenAHhelper as helper
 class yell_log(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.yellbot_task.start()
+        # toggleable var for logging yells sent
+        self.log_yells = True
+        self.counter = 0
+        self.yell_history = [{} for i in range(31)]
 
     @commands.command()
     async def yells(self, ctx, message: str):
@@ -34,3 +39,42 @@ class yell_log(commands.Cog):
                 await ctx.send(f'{ctx.author.mention}' +
                                ' you must have "manage messages" permissions to use this command!')
             return
+
+    @tasks.loop(seconds=30)
+    async def yellbot_task(self):
+        await self.bot.wait_until_ready()
+        try:
+            if helper.check_channels_exist() and helper.check_connection(helper.yell_url):
+                print(f"[{helper.get_my_timestamp_now()}] cycle started.")
+                channel_ids = helper.get_yell_channels()
+                if self.log_yells:
+                    log = open('eden_yell_log.txt', 'a')
+
+                yell_tuple = helper.get_new_yells(self.yell_history)
+                self.yell_history = yell_tuple[1]
+                for yell in yell_tuple[0]:
+                    yell_message = helper.yell_formatter(yell)
+
+                    if self.log_yells:
+                        log.write(yell_message + '\n')
+
+                    for channel in channel_ids:
+                        try:
+                            to_send = self.bot.get_channel(channel)
+                            await to_send.send(yell_message)
+                        except AttributeError:
+                            print(sys.exc_info())
+                            helper.del_yell_channel(channel)
+                            print(f'Purged invalid channel {channel}')
+                self.counter += 1
+                print(f'[{helper.get_my_timestamp_now()}] cycle {self.counter} complete.')
+                log.close()
+            else:
+                print(f'[{helper.get_my_timestamp_now()}] cycle skipped.' +
+                      ' Either no channels are registered or the site is down.')
+                # wait an addition 30 sec when server is possibly down
+                await asyncio.sleep(30)
+        except Exception:
+            print('Background task raised exception')
+            print(sys.exc_info())
+            raise
