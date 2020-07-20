d985e81b90a809a2eed2819b6d07f34adf3e78da
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-20 14:36:02 -0700

    added class misc with command tnl
    tnl command can be used to get the exp from the beginning of a level to the end of another level

--- bot.py
+++ bot.py
@@ -67,6 +67,29 @@ async def args(ctx, *, message: str):
         await ctx.send(message[item[0]])
 
 
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['exp'])
+    async def tnl(self, ctx, *, message: str):
+        '''Get the exp from the beginning of a certain level to the end of another.
+        Usage: !tnl [initial level] [target level]'''
+        lvls = message.split(' ')
+        print(lvls)
+        try:
+            initial_level = int(lvls[0])
+            target_level = int(lvls[1])
+            exp_msg = helper.get_tnl(initial_level, target_level)
+
+            await ctx.send(exp_msg)
+
+        except ValueError:
+            await ctx.send('Levels must be provided as numbers (like 1 or 30)')
+        except IndexError:
+            await ctx.send('Must provide two levels.')
+
+
 class Player(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -148,6 +171,7 @@ class Market(commands.Cog):
 # add created cogs
 bot.add_cog(Player(bot))
 bot.add_cog(Market(bot))
+bot.add_cog(Misc(bot))
 bot.add_cog(yellbot.yell_log(bot))
 
 try:
--- edenAHhelper.py
+++ edenAHhelper.py
@@ -166,6 +166,83 @@ item_prefixes = [
     'bunch_of_',
     'remnant_of_a_'
 ]
+exp_tnl = {
+    1: 500,
+    2: 750,
+    3: 1000,
+    4: 1250,
+    5: 1500,
+    6: 1750,
+    7: 2000,
+    8: 2200,
+    9: 2400,
+    10: 2600,
+    11: 2800,
+    12: 3000,
+    13: 3200,
+    14: 3400,
+    15: 3600,
+    16: 3800,
+    17: 4000,
+    18: 4200,
+    19: 4400,
+    20: 4600,
+    21: 4800,
+    22: 5000,
+    23: 5100,
+    24: 5200,
+    25: 5300,
+    26: 5400,
+    27: 5500,
+    28: 5600,
+    29: 5700,
+    30: 5800,
+    31: 5900,
+    32: 6000,
+    33: 6100,
+    34: 6200,
+    35: 6300,
+    36: 6400,
+    37: 6500,
+    38: 6600,
+    39: 6700,
+    40: 6800,
+    41: 6900,
+    42: 7000,
+    43: 7100,
+    44: 7200,
+    45: 7300,
+    46: 7400,
+    47: 7500,
+    48: 7600,
+    49: 7700,
+    50: 7800,
+    51: 8000,
+    52: 9200,
+    53: 10400,
+    54: 11600,
+    55: 12800,
+    56: 14000,
+    57: 15200,
+    58: 16400,
+    59: 17600,
+    60: 18800,
+    61: 20000,
+    62: 21500,
+    63: 23000,
+    64: 24500,
+    65: 26000,
+    66: 27500,
+    67: 29000,
+    68: 30500,
+    69: 32000,
+    70: 34000,
+    71: 36000,
+    72: 38000,
+    73: 40000,
+    74: 42000,
+    75: 44000
+}
 
 
 def init_item_names():
@@ -270,6 +347,17 @@ def build_yell_embed():
         embed.add_field(name='placeholder', value=entry)
     print(len(yell_info))
     return embed
+
+
+def get_tnl(init_lv, target_lv):
+    if init_lv not in exp_tnl or target_lv not in exp_tnl:
+        return "Invalid levels provided."
+    if init_lv > target_lv:
+        return "Target level less than initial level."
+    tnl = 0
+    for level in range(init_lv, target_lv):
+        tnl += exp_tnl[level]
+    return f"Exp from Level {init_lv} to Level {target_lv} is {tnl}."
 
 
 def format_player_name(name):
