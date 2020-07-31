aef60a7577503f62e62fcf570684e9bfc76b5dd3
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-07-31 09:42:32 -0700

    filter string given to !crafts helpers

--- edenbotcogs/cogs/Player.py
+++ edenbotcogs/cogs/Player.py
@@ -25,8 +25,9 @@ class Player(commands.Cog):
     async def crafts(self, ctx, message: str):
         '''Get a player's crafting levels.
             Usage: !crafts [player]'''
-        if check_player_exist(message):
-            crafts = get_player_crafts(message)
-            await ctx.send(embed=build_crafts_embed(message, crafts))
+        player_name = format_player_name(message)
+        if check_player_exist(player_name):
+            crafts = get_player_crafts(player_name)
+            await ctx.send(embed=build_crafts_embed(player_name, crafts))
         else:
             await ctx.send('Player not found.')
