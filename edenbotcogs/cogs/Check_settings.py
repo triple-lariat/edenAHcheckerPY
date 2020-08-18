98547600c66f438ac1a05d05396cc01646804066
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-08-18 09:59:18 -0700

    build commands for enabling and disabling categories of commands

--- edenbotcogs/cogs/Check_settings.py
+++ edenbotcogs/cogs/Check_settings.py
@@ -3,6 +3,7 @@
 # You may also find me on Eden or Eden's discord under the name Tranquille
 
 from discord.ext import commands
+from edenbotcogs.coghelpers.settings_helper import *
 
 
 class settings(commands.Cog):
@@ -12,6 +13,40 @@ class settings(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def disablecommands(self, ctx):
+    async def disableall(self, ctx):
         '''Disables usage of all commands by anyone without "Manage messages" permissions in the current channel.'''
+        disable_all_commands(ctx.channel.id)
+        await ctx.send('All commands have been disabled for this channel!')
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def disablecommand(self, ctx, message: str):
+        try:
+            message = message.lower()
+            flag = command_options[message]
+            disable_command(ctx.channel.id, flag)
+        except KeyError:
+            await ctx.send('Invalid command category given.')
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def enableall(self, ctx):
+        '''Enables all disabled commands, if any, in the current channel.'''
+        enable_all_commands(ctx.channel.id)
+        await ctx.send('All commands have been enabled for this channel!')
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def enablecommand(self, ctx, message: str):
+        try:
+            message = message.lower()
+            flag = command_options[message]
+            enable_command(ctx.channel.id, flag)
+        except KeyError:
+            await ctx.send('Invalid command category given.')
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def listdisabled(self, ctx):
+        await ctx.send(disabled_channels)
 
