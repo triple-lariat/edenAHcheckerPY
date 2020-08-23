c099b066b80e4ca3865e86e0a17df791f191466f
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-08-22 22:04:32 -0700

    make new/fullmoon commands look nicer

--- edenbotcogs/cogs/Timers.py
+++ edenbotcogs/cogs/Timers.py
@@ -46,14 +46,16 @@ class Timers(commands.Cog):
     async def fullmoon(self, ctx):
         '''Gives the time to next full moon.
         Usage: !fullmoon'''
-        await ctx.send(get_next_end_moon(True, ctx.message.guild.id))
+        next_full = get_next_end_moon(True, ctx.message.guild.id)
+        await ctx.send(f'Next full moon at: {next_full}')
 
     @check_channel(ID)
     @commands.command()
     async def newmoon(self, ctx):
         '''Gives the time to next new moon
         Usage: !newmoon'''
-        await ctx.send(get_next_end_moon(False, ctx.message.guild.id))
+        next_new = get_next_end_moon(False, ctx.message.guild.id)
+        await ctx.send(f'Next new moon at: {next_new}')
 
     @check_channel(ID)
     @commands.command()
